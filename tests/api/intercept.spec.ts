d26177dcb4f38b47f2ebfb5a1d25c22818c31435
Abhilasha <abhilasha.varshney@gmail.com>
2026-07-01 07:57:13 +0530

    latest

--- tests/api/intercept.spec.ts
+++ tests/api/intercept.spec.ts
@@ -1,39 +1,39 @@
-//web app--->intercept the network calls and log them 
-//**==wildcard--matched all the URLs...
-import{test,expect} from '@playwright/test'
+// //web app--->intercept the network calls and log them 
+// //**==wildcard--matched all the URLs...
+// import{test,expect} from '@playwright/test'
 
-//intercept the network calls ..
-test('intercept and log requests',async({page})=>{
-  await page.route('**/*',async(route)=>{
+// //intercept the network calls ..
+// test('intercept and log requests',async({page})=>{
+//   await page.route('**/*',async(route)=>{
 
-  console.log(route.request().method(),route.request().url());
-  await route.continue();//url1--capture and continue../..url2---capture----continue..
-  });
+//   console.log(route.request().method(),route.request().url());
+//   await route.continue();//url1--capture and continue../..url2---capture----continue..
+//   });
   
-  await page.goto('https://naveenautomationlabs.com/opencart/index.php?route=common/home',{ waitUntil: 'domcontentloaded' });
+//   await page.goto('https://naveenautomationlabs.com/opencart/index.php?route=common/home',{ waitUntil: 'domcontentloaded' });
 
 
-})
+// })
 
-//intercept with mocking.:
-//mocking:fake data/response
-test('mock search data aapi',async({page})=>{
+// //intercept with mocking.:
+// //mocking:fake data/response
+// test('mock search data aapi',async({page})=>{
 
-     let fakeProducts=[
-        { name:'Fake Macbook Pro',price:"$599"},
-        { name:'Fake iphone 20',price:"$999"}
+//      let fakeProducts=[
+//         { name:'Fake Macbook Pro',price:"$599"},
+//         { name:'Fake iphone 20',price:"$999"}
 
-     ] ;
-     await page.route('**/index.php?route=product/search&search=macbook',(route)=>{
+//      ] ;
+//      await page.route('**/index.php?route=product/search&search=macbook',(route)=>{
 
-        route.fulfill({
-            status:200,
-            contentType:'application/json',
-            body:JSON.stringify(fakeProducts),
-        });
+//         route.fulfill({
+//             status:200,
+//             contentType:'application/json',
+//             body:JSON.stringify(fakeProducts),
+//         });
 
-     });
-         await page.goto('https://naveenautomationlabs.com/index.php?route=product/search&search=macbook');
+//      });
+//          await page.goto('https://naveenautomationlabs.com/index.php?route=product/search&search=macbook');
 
-      await page.pause();
-});
+//       await page.pause();
+// });
